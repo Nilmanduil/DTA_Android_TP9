a2bb93c290ab34c4ca30958153a1489e19f53437
Thibault Goudouneix <nilmanduil@gmail.com>
2017-10-12 17:21:03 +0200

    Save bundle

--- app/src/main/java/fr/codevallee/formation/android_tp9/EditActivity.java
+++ app/src/main/java/fr/codevallee/formation/android_tp9/EditActivity.java
@@ -2,6 +2,7 @@ package fr.codevallee.formation.android_tp9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditActivity extends Traceur {
 
@@ -9,5 +10,15 @@ public class EditActivity extends Traceur {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        EditText text = (EditText) findViewById(R.id.editText);
+        text.setText(savedInstanceState.getString("text"));
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        EditText text = (EditText) findViewById(R.id.editText);
+        outState.putString("text", text.getText().toString() + " saved");
     }
 }
